28758f0bb87d4d6d3cbcd28de76ed5296e806530
Norio4 <Norio4>
2021-08-31 20:25:33 +0900

    Update todos migration

--- migrations/2021-08-22-125515_todos/down.sql
+++ migrations/2021-08-22-125515_todos/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE todo;
+DROP TABLE todos;
--- migrations/2021-08-22-125515_todos/up.sql
+++ migrations/2021-08-22-125515_todos/up.sql
@@ -1,5 +1,8 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS todos (
     id SERIAL PRIMARY KEY,
-    text TEXT NOT NULL
+    user_id Integer NOT NULL.
+    text TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 );
